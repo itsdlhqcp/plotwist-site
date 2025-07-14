bdd01f4aa07bcc9fdf3cd4cb8b8396ea2062e4c9
itsdlhqcp <dilhaqued@gmail.com>
2025-07-14 17:20:37 +0530

    added site app link

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -6,10 +6,10 @@ import { InfiniteMovingCards } from "./ui/InfiniteCards";
 const Clients = () => {
   return (
     <section id="testimonials" className="py-20">
-      <h1 className="heading">
+      {/* <h1 className="heading">
         Rave Comments from
         <span className="text-purple"> Our Audience</span>
-      </h1>
+      </h1> */}
 
       <div className="flex flex-col items-center max-lg:mt-10">
         <div
